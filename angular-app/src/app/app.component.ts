80b1114fee5d8564dee203a658ef513bc112ecde
anhnv <anhnv17@vietinbank.vn>
2024-04-17 16:45:26 +0700

    route

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import logo from '../assets/angular.png'
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   myLogo = logo
+
+  constructor(private router: Router) {
+    console.log('app1 - components')
+  }
+
+  navigateToMy() {
+    //this.router.navigateByUrl('/standalone/by').then(r => {}); // Đường dẫn đến route 'home'
+    this.router.navigate(['/standalone/my']).then(r => {
+      console.log('navigateToMy')
+    }); // Sử dụng navigate
+  }
 }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,25 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {RouterModule, RouterOutlet, Routes} from "@angular/router";
 import {CardComponent} from "./card/card.component";
+import {STANDALONE_COMPONENTS_ROUTES} from "./remote-routes";
 
 const routes: Routes = [
   {
-    path: 'card',
-    component: CardComponent
-  }
+    path: 'standalone',
+    loadChildren: () =>
+      import('./standalone-component.route')
+        .then(m => m.STANDALONE_COMPONENTS_ROUTES)
+  },
 ];
+
+// const routes: Routes = [
+//   {
+//     path: 'card',
+//     component: CardComponent
+//   },
+//   ...STANDALONE_COMPONENTS_ROUTES,
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     RouterOutlet,
-    RouterModule.forChild(routes)
+    // RouterModule.forChild(routes)
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    console.log('App1 - module')
+  }
+}
